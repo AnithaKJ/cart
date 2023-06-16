e1b3abd0cefff0e46e1a42e4d137d0377f2bb602
Anitha K J <136343677+AnithaKJ@users.noreply.github.com>
2023-06-16 08:41:21 +0530

    Add files via upload

---
+++ navcart.php
@@ -0,0 +1,12 @@
+<nav>
+        <div class="navbar" id="navbar">
+            <div class="nav" id="nav">
+                <div class="logo" id="top-nav"><a href="index.php"><img src=".//images/logo.png" height="60px"></a></div>
+                <div class="sayatitle" id="top-nav"><span class="saya">SAYA ENTERPRISES</span><br><span class="slogan">Sales and Services of Agricultural Machineries and Implements</span> </div>
+                <div class="cart" id="top-nav"><a href="./cart1.php"><i class="fa-solid fa-cart-shopping fa-2x"></i></a></div>
+                <div class="login" id="top-nav"><h4 class="log"><a href="login.php">LOGIN / SIGN UP</a></h4></div>
+                <!-- <a href="#">LOGIN</a> -->
+            </div>
+        </div>
+</nav>
+    
